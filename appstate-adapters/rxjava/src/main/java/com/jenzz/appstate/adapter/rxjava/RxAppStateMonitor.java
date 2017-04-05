813e6ba0f4c86daf1b1c676423fc656a932cbdfd
Jens Driller <jens@jensdriller.com>
2017-04-05 14:35:26 +0100

    Update RxJava adapter to v1.2.9

--- appstate-adapters/rxjava/src/main/java/com/jenzz/appstate/adapter/rxjava/RxAppStateMonitor.java
+++ appstate-adapters/rxjava/src/main/java/com/jenzz/appstate/adapter/rxjava/RxAppStateMonitor.java
@@ -7,20 +7,36 @@ import com.jenzz.appstate.AppState;
 import com.jenzz.appstate.adapter.rxjava.internal.AppStateEmitter;
 import com.jenzz.appstate.internal.DefaultAppStateRecognizer;
 
+import rx.Emitter;
 import rx.Observable;
 
-import static rx.Emitter.BackpressureMode.LATEST;
+import static rx.Emitter.BackpressureMode.NONE;
 
 public final class RxAppStateMonitor {
+
+    /**
+     * Calls {@link #monitor(Application, rx.Emitter.BackpressureMode)}
+     * with {@link rx.Emitter.BackpressureMode#NONE Emitter.BackpressureMode.NONE}.
+     *
+     * @param app the application to monitor for app state changes
+     * @return a new {@link Observable} that emits app state changes
+     */
+    @NonNull
+    public static Observable<AppState> monitor(@NonNull Application app) {
+        return monitor(app, NONE);
+    }
 
     /**
      * Creates a new {@link Observable} that emits {@link AppState} items
      * whenever the app goes into background and comes back into foreground.
      *
-     * @return a new {@link Observable}
+     * @param app the application to monitor for app state changes
+     * @param backpressureMode the backpressure mode to apply if the downstream Subscriber doesn't request (fast) enough
+     * @return a new {@link Observable} that emits app state changes
      */
     @NonNull
-    public static Observable<AppState> monitor(@NonNull Application app) {
-        return Observable.fromEmitter(new AppStateEmitter(new DefaultAppStateRecognizer(app)), LATEST);
+    public static Observable<AppState> monitor(@NonNull Application app, @NonNull
+        Emitter.BackpressureMode backpressureMode) {
+        return Observable.create(new AppStateEmitter(new DefaultAppStateRecognizer(app)), backpressureMode);
     }
 }
--- appstate-adapters/rxjava/src/test/java/com/jenzz/appstate/adapter/rxjava/internal/AppStateEmitterTest.java
+++ appstate-adapters/rxjava/src/test/java/com/jenzz/appstate/adapter/rxjava/internal/AppStateEmitterTest.java
@@ -15,10 +15,12 @@ import rx.Emitter;
 import rx.Observable;
 import rx.functions.Cancellable;
 
+import static com.jenzz.appstate.AppState.BACKGROUND;
+import static com.jenzz.appstate.AppState.FOREGROUND;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
-import static rx.Emitter.BackpressureMode.NONE;
+import static rx.Emitter.BackpressureMode.ERROR;
 
 public class AppStateEmitterTest {
 
@@ -35,21 +37,21 @@ public class AppStateEmitterTest {
     initMocks(this);
 
     emitter.call(mockEmitter);
-    emitterObservable = Observable.fromEmitter(new AppStateEmitter(mockAppStateRecognizer), NONE);
+    emitterObservable = Observable.create(new AppStateEmitter(mockAppStateRecognizer), ERROR);
   }
 
   @Test
   public void emitsForeground() {
     stubRecognizer.notifyAppDidEnterForeground();
 
-    verify(mockEmitter).onNext(AppState.FOREGROUND);
+    verify(mockEmitter).onNext(FOREGROUND);
   }
 
   @Test
   public void emitsBackground() {
     stubRecognizer.notifyAppDidEnterBackground();
 
-    verify(mockEmitter).onNext(AppState.BACKGROUND);
+    verify(mockEmitter).onNext(BACKGROUND);
   }
 
   @Test
